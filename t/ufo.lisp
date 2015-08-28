aa8d025acc386b2663493ba8163d0e8f96c1e2c2
Masaya TANIGUCHI <ta2gch@gmail.com>
2015-08-29 01:11:33 +0900

    test gh://

--- t/ufo.lisp
+++ t/ufo.lisp
@@ -9,6 +9,7 @@
 (ok (not (ufo:ufo "install" "ql://cl-gists")))
 (ok (not (ufo:ufo "remove" "repl")))
 (ok (not (ufo:ufo "update" "gist://ta2gch/repl.ros")))
+(ok (not (ufo:ufo "install" "gh://ta2gch/cl-pov")))
 (ok (not (ufo:ufo "addon-install"
 		  (format nil "file://~a"
 			  (merge-pathnames
@@ -16,7 +17,6 @@
 			   (ql:where-is-system :ufo))))))
 (ok (not (ufo:ufo "addon-remove" "build")))
 (ok (not (ufo:ufo "help")))
-
 
 (ok (ufo:ufo "install" "gist://ta2gch/repl"))
 (ok (ufo:ufo "install" "file://ta2gch/repl"))
