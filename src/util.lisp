a2d8bda34f41a370431476e3a0b6e30ddec3a90a
Masaya TANIGUCHI <ta2gch@gmail.com>
2015-08-29 10:58:42 +0900

    use trivial-download

--- src/util.lisp
+++ src/util.lisp
@@ -1,7 +1,7 @@
 (in-package #:cl-user)
 (defpackage ufo.util
   (:use :cl)
-  (:export :dot-ufo :dot-roswell :download))
+  (:export :dot-ufo :dot-roswell))
 (in-package #:ufo.util)
 
 (defun dot-ufo (path)
@@ -10,14 +10,3 @@
 (defun dot-roswell (path)
   (merge-pathnames
    path (merge-pathnames #p".roswell/" (user-homedir-pathname))))
-
-(defun download (url dist)
-  (with-open-file (file dist
- 			:direction :io
- 			:if-does-not-exist :create
- 			:if-exists :supersede
- 			:element-type '(unsigned-byte 8))
-     (let ((input (dex:get url :want-stream t :force-binary t)))
-       (do ((byte (read-byte input nil nil) (read-byte input nil nil)))
- 	  ((not byte))
- 	(write-byte byte file)))))
