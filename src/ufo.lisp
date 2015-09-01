fcd38f77b041278334b19325804ff392e9e4032d
Masaya TANIGUCHI <ta2gch@gmail.com>
2015-09-01 12:01:35 +0900

    slectable sb-ext or uiop

--- src/ufo.lisp
+++ src/ufo.lisp
@@ -1,29 +1,31 @@
 (in-package #:cl-user)
 (defpackage ufo
-  (:use :cl :uiop :ufo.util :ufo.addon)
+  (:use :cl :ufo.util :ufo.addon)
   (:export :ufo :setup :dot-ufo :dot-roswell))
 (in-package #:ufo)
 
 (defun subcmd-p (subcmd)
   (let* ((ros (make-pathname :name subcmd))
-	 (cmd-path (merge-pathnames ros (ufo.util:dot-ufo #p"addon/"))))
+	 (cmd-path (merge-pathnames ros (dot-ufo #p"addon/"))))
     (if (probe-file cmd-path) cmd-path nil)))
 
 (defun setup ()
-  (ensure-directories-exist (ufo.util:dot-ufo #p"addon/"))
-  (ensure-directories-exist (ufo.util:dot-ufo #p"tmp/"))
+  (ensure-directories-exist (dot-ufo #p"addon/"))
+  (ensure-directories-exist (dot-ufo #p"tmp/"))
   (let ((addon-dir(merge-pathnames "addon/"(ql:where-is-system :ufo))))
     (dolist (addon '("addon-install.ros" "addon-remove.ros"
 		     "install.ros" "remove.ros" "update.ros" "help.ros"))
-      (ufo.addon:addon(merge-pathnames addon addon-dir)))))
+      (addon (merge-pathnames addon addon-dir)))))
 
 (defun ufo (subcmd &rest argv)
   (setup)
   (handler-case 
       (let ((cmd (subcmd-p subcmd)))
 	(if cmd
-	    (run-program (cons cmd argv)
-	     :output t :error-output *error-output*)
+	    #+sbcl
+	    (sb-ext:run-program cmd argv :output t :input t)
+	    #-sbcl
+	    (uiop:run-program cmd argv :output t :error-output t :input t)
 	    (progn (princ "unkown subcommand") t)))
     (error (e) (princ e) (terpri) t)))
   
