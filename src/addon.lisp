b17105f7dfef0b5612bb58065ed04eddf5c32ec0
Masaya TANIGUCHI <ta2gch@gmail.com>
2015-09-02 21:12:38 +0900

    fix src/addon.lisp: use uiop:run-program

--- src/addon.lisp
+++ src/addon.lisp
@@ -1,6 +1,6 @@
 (in-package #:cl-user)
 (defpackage ufo.addon
-  (:use :cl :ufo.util)
+  (:use :cl :uiop :ufo.util)
   (:export :addon))
 (in-package #:ufo.addon)
 
@@ -8,4 +8,5 @@
   (let* ((ros (first argv))
 	 (out (merge-pathnames (pathname-name ros) (dot-ufo #p"addon/"))))
     (unless (probe-file out)
-      (ros:roswell`("build",ros"-o",out):interactive nil))))
+      (run-program `("ros" "build",(namestring ros)"-o",(namestring out))
+		   :output t :error-output t))))
